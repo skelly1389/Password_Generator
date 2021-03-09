732b8a07d6cadac46d597243c47b9f4a145c4a61
Sean Kelly <invertedrdm@gmail.com>
2021-03-09 02:25:31 -0500

    password is now written to html instead of in alert box

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-// sets different selectable characters to their own strings
+// sets different selectable character sets to their own strings
 const lowerChar = "abcdefghijklmnopqrstuvwxyz";
 const upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberChar = "1234567890";
@@ -37,7 +29,6 @@ function makeArray() {
     generatePassword = generatePassword.concat(numChar);
   }
 }
-
 // the function that takes the character set array and uses it to create a password
 function makePass(){
   // clears stored password in case function is reset due to not meeting parameters
@@ -50,13 +41,11 @@ function makePass(){
     numCh: confirm("Press okay to include numbers"),
     spChar: confirm("Press okay to include special characters")
   }
-
   // makes sure the password is between 8 and 128 characters in length
-  while (passQual.passLength < 8) {
+  while (passQual.passLength < 8 || passQual.passLength > 128) {
     alert("Password length must be between 8 and 128 characters and contain at least one set of characters!");
     passQual.passLength = prompt("Choose a password length between 8 and 128 characters!");
   }
-
   // makes sure at least one set of characters is selected
   while (!passQual.lowCase && !passQual.upCase && !passQual.numCh && !passQual.spChar) {
     alert("Must select at least one set of characters!");
@@ -65,16 +54,13 @@ function makePass(){
     passQual.numCh = confirm("Press okay to include numbers");
     passQual.spChar = confirm("Press okay to include special characters");
   }
-
   makeArray();
   // makes sure password length is equal to user parameter
   for (i = 0; i < passQual.passLength; i++){
     generatedPassword.push(generatePassword[Math.floor(Math.random() * generatePassword.length)])
   }
-
   // turns the generated password array into a string without commas
   let password = generatedPassword.join("");
-
   // checks that, if a parameter is set to true, the password must contain a character from that set
   if((passQual.lowCase == true && generatedPassword.some(item => lowChar.includes(item)) == false) 
     || (passQual.upCase == true && generatedPassword.some(item => upChar.includes(item)) == false)
@@ -85,8 +71,11 @@ function makePass(){
       return;
     // if the password includes at least one character from selected parameters password is displayed
     } else {  
-        alert("Your password is: " + password);
+        // replaces html text at id password with new variable
+        let passwordText = document.querySelector("#password");
+        // sets string to generated password
+        passwordText.value = password;
       }
   }
-
+// runs function makePass when button on page is clicked
 generateBtn.addEventListener("click", makePass);
